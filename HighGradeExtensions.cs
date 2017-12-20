fe913c94d32a1e21367e8c23e8aeb4aa6ce81615
Jackie-Young7 <917722872@qq.com>
2017-12-20 21:39:42 +0800

    init

--- HighGradeExtensions.cs
+++ HighGradeExtensions.cs
@@ -1,14 +1,15 @@
 ﻿using Xmu.Crms.Services.HighGrade;
+using Xmu.Crms.Services.Insomnia;
 using Xmu.Crms.Shared.Service;
 
 namespace Microsoft.Extensions.DependencyInjection
 { 
-    public static class Group1Extensions
+    public static class HighGradeExtensions
     {
         // 为每一个你写的Service写一个这样的函数，把 UserService 替换为你实现的 Service
-        public static IServiceCollection AddGroup1UserService(this IServiceCollection serviceCollection)
+        public static IServiceCollection AddHighGradeTimerService(this IServiceCollection serviceCollection)
         {
-            return serviceCollection.AddScoped<IUserService, UserService>();
+            return serviceCollection.AddScoped<ITimerService, TimerService>();
         }
     }
 }
